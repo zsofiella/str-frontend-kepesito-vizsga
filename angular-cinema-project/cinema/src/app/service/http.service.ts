fae6ed3a0bb6d5674ac24c39abef0776c09f2637
Galambvári Zsófia <galambvarizsofia@gmail.com>
2021-04-30 12:49:06 +0200

    API changed to BASE

--- angular-cinema-project/cinema/src/app/service/http.service.ts
+++ angular-cinema-project/cinema/src/app/service/http.service.ts
@@ -1,25 +1,24 @@
+  
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Movie } from '../model/movie';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  apiUrl: string = 'https://testserver/zsofiella/movies';
+  BASE_URL = 'https://tr360-frontend-exam-april.azurewebsites.net/zsofiella/movies';
 
-  constructor(
-    private http: HttpClient
-  ) { }
 
-  getMovieList(): Observable<Movie[]> {
-    return this.http.get<Movie[]>(this.apiUrl);
+  constructor( private http: HttpClient) { }
+
+  getMovieList():Observable<Movie[]> {
+    return this.http.get<Movie[]>(this.BASE_URL);
   }
 
-  deleteMovie(id):any {
-    return null;
+  deleteMovie(id: number):Observable<Movie> {
+    return this.http.delete<Movie>(`${this.BASE_URL}/${id}`);
   }
 }

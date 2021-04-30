7cee06331b137775267e7683208cf369691da1d2
Galambvári Zsófia <galambvarizsofia@gmail.com>
2021-04-30 12:32:25 +0200

    changes

--- angular-cinema-project/cinema/src/app/service/http.service.ts
+++ angular-cinema-project/cinema/src/app/service/http.service.ts
@@ -1,16 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Movie } from '../model/movie';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  BASE_URL = 'http://localhost:3000/movies';
+  apiUrl: string = 'https://testserver/zsofiella/movies';
 
-  constructor(undefined) { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
-  getMovieList():any {
-    return null;
+  getMovieList(): Observable<Movie[]> {
+    return this.http.get<Movie[]>(this.apiUrl);
   }
 
   deleteMovie(id):any {
